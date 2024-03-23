5328c6818eb240f209d843e8b36062a7d78676a2
Aarav Purohit <purohit.aarav39@gmail.com>
2024-03-23 12:50:47 -0400

    latest

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException, status, Security, FastAPI
 from fastapi.security import APIKeyHeader, APIKeyQuery
+import json
+import uvicorn
 
 api_keys = [
     "my_api_key"
@@ -35,6 +37,17 @@ def private(api_key: str = Security(get_api_key)):
 
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
+def main():
+    api_url = "https://apimdev.wakefern.com/mockexample/V1/getItemDetails"  # Replace this with your API endpoint
+    subscription_key = "4ae9400a1eda4f14b3e7227f24b74b44"  # Replace this with your subscription key
+    json_data = get_api_key(api_url, subscription_key)
+    if json_data:
+        # Process the JSON data as needed
+        print(json.dumps(json_data, indent=4))  # Example: printing the JSON data
+
+        # You can further process the JSON data here...
+
+if __name__ == "__main__":
+    main()
