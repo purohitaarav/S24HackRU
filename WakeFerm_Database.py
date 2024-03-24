270b09f8adcedd0ab3468bb0c2ebb05be2d802a0
Aman Grandhi <agrandhi4@gmail.com>
2024-03-23 23:33:59 -0400

    Almighty push

--- WakeFerm_Database.py
+++ WakeFerm_Database.py
@@ -35,8 +35,15 @@ def get_store_details():
     header = {'Ocp-Apim-Subscription-Key': '4ae9400a1eda4f14b3e7227f24b74b44', "User-Agent": "PostmanRuntime/7.36.3",
                "Content-Type": "application/json"}
     response = requests.get(url, headers=header)
-    print(response)
-    return response.text
+    data = json.loads(response.content)
+    # Check if 'hits' key exists and it has at least one item
+    if 'hits' in data and len(data['hits']) > 0:
+        # Extract ingredients from the first recipe
+        Description = data['hits'][0]['item']['d']
+        return Description
+    else:
+        return {"message": "No item found"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
